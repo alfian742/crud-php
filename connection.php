f9b55dd951c0f1b111205f631a8210ab3eb54fb0
ALFIAN HIDAYATULLAH <122254465+alfian742@users.noreply.github.com>
2023-10-20 21:26:22 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,11 @@
 <?php
 
-$db = mysqli_connect("localhost", "root", "", "mahasiswa");
+$server      = "localhost";
+$username    = "root";
+$password    = "";
+$database    = "siakad";
+
+$db = mysqli_connect($server, $username, $password, $database);
 
 if (!$db) {
     die("Koneksi ke database gagal: " . mysqli_connect_error($db));
